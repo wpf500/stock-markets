65793cdcb8bcaf967e31e189d419010daf962a10
Will Franklin <will.franklin@guardian.co.uk>
2015-08-25 11:50:04 +0100

    Tick cacher

--- cache.js
+++ cache.js
@@ -20,32 +20,37 @@ function putObject(key, body) {
     return Q.nfcall(s3putObject, params);
 }
 
-var ranges = [
-    [moment.utc().startOf('day'), moment.utc(), moment.utc().format('YYYY-MM-DD')],
-    [moment.utc().subtract(1, 'day'), moment.utc(), '24hr'],
-    [moment.utc().subtract(1, 'hour'), moment.utc(), '1hr'],
-    [moment.utc().subtract(2, 'hours'), moment.utc(), '2hr']
-];
+function tick() {
+    var ranges = [
+        [moment.utc().startOf('day'), moment.utc(), moment.utc().format('YYYY-MM-DD')],
+        [moment.utc().subtract(1, 'day'), moment.utc(), '24hr'],
+        [moment.utc().subtract(1, 'hour'), moment.utc(), '1hr'],
+        [moment.utc().subtract(2, 'hours'), moment.utc(), '2hr']
+    ];
 
-console.log(moment.utc().format());
+    console.log(moment.utc().format());
 
-ranges.forEach(function (range) {
-    common.indices(range[0], range[1]).then(function (dates) {
-        var indices = {};
-        dates.forEach(function (date) {
-            date.indices.forEach(function (index) {
-                if (!indices[index.name]) indices[index.name] = [];
-                indices[index.name].push({'date': date.date, 'indices': [index]});
+    ranges.forEach(function (range) {
+        common.indices(range[0], range[1]).then(function (dates) {
+            var indices = {};
+            dates.forEach(function (date) {
+                date.indices.forEach(function (index) {
+                    if (!indices[index.name]) indices[index.name] = [];
+                    indices[index.name].push({'date': date.date, 'indices': [index]});
+                });
             });
-        });
 
-        var allPromise = putObject('all_' + range[2], JSON.stringify(dates));
-        var indexPromises = Object.keys(indices).map(function (indexName) {
-            return putObject(indexName + '_' + range[2], JSON.stringify(indices[indexName]));
-        });
+            var allPromise = putObject('all_' + range[2], JSON.stringify(dates));
+            var indexPromises = Object.keys(indices).map(function (indexName) {
+                return putObject(indexName + '_' + range[2], JSON.stringify(indices[indexName]));
+            });
 
-        return Q.all(indexPromises.concat([allPromise]));
-    }).then(function () {
-        console.log('Put objects complete');
-    }).done();
-});
+            return Q.all(indexPromises.concat([allPromise]));
+        }).then(function () {
+            console.log(' ', range[2], 'complete');
+        }).done();
+    });
+}
+
+setInterval(tick, 60000);
+tick();
